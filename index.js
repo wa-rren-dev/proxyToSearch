e42a0e4c2ac7fccaba2d0bc09f0668ae06f99ad8
Warren Keith <w@rren.dev>
2020-06-09 14:04:50 +0100

    Now working but only for one result because hey why not

--- index.js
+++ index.js
@@ -1,45 +1,62 @@
 var http = require("http");
 var httpProxy = require("http-proxy");
 
-const port = 5000;
+const port = 6123;
 
-var proxy = httpProxy.createProxyServer({ selfHandleResponse: true });
+var proxy = httpProxy.createProxyServer({
+  selfHandleResponse: true,
+});
 
 function firstCharToLowercase(string) {
-	return string.charAt(0).toLowerCase() + string.slice(1);
+  return string.charAt(0).toLowerCase() + string.slice(1);
 }
 
 function ConvertKeysToLowerCase(obj) {
-	var output = {};
-	for (i in obj) {
-		if (Object.prototype.toString.apply(obj[i]) === "[object Object]") {
-			output[firstCharToLowercase(i)] = ConvertKeysToLowerCase(obj[i]);
-		} else if (Object.prototype.toString.apply(obj[i]) === "[object Array]") {
-			output[firstCharToLowercase(i)] = [];
-			output[firstCharToLowercase(i)].push(ConvertKeysToLowerCase(obj[i][0]));
-		} else {
-			output[firstCharToLowercase(i)] = obj[i];
-		}
-	}
-	return output;
+  var output = {};
+  for (i in obj) {
+    if (Object.prototype.toString.apply(obj[i]) === "[object Object]") {
+      output[firstCharToLowercase(i)] = ConvertKeysToLowerCase(obj[i]);
+    } else if (Object.prototype.toString.apply(obj[i]) === "[object Array]") {
+      output[firstCharToLowercase(i)] = [];
+      output[firstCharToLowercase(i)].push(ConvertKeysToLowerCase(obj[i][0]));
+    } else {
+      output[firstCharToLowercase(i)] = obj[i];
+    }
+  }
+  return output;
 }
 
 var server = http.createServer(function (req, res) {
-	proxy.on("proxyRes", function (proxyRes, req, res) {
-		var body = [];
-		proxyRes.on("data", function (chunk) {
-			body.push(chunk);
-		});
-		proxyRes.on("end", function () {
-			body = Buffer.concat(body).toString();
-			var bodyJSON = JSON.parse(body);
-			res.end(JSON.stringify(ConvertKeysToLowerCase(bodyJSON)));
-		});
-	});
-	proxy.web(req, res, {
-		target: "https://cks.nice.org.uk",
-		changeOrigin: true,
-	});
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Request-Method", "*");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
+  proxy.on("proxyRes", function (proxyRes, req, res) {
+    var body = [];
+    proxyRes.on("data", function (chunk) {
+      body.push(chunk);
+    });
+    proxyRes.on("end", function () {
+      body = Buffer.concat(body).toString();
+      var bodyJSON;
+      try {
+        bodyJSON = JSON.parse(body);
+        console.log("parsed body");
+        res.end(JSON.stringify(ConvertKeysToLowerCase(bodyJSON)));
+      } catch (e) {
+        console.error(e);
+      }
+    });
+  });
+  proxy.web(req, res, {
+    target: "https://cks.nice.org.uk",
+    changeOrigin: true,
+  });
 });
 
 console.log(`Proxy listening on port ${port}`);
